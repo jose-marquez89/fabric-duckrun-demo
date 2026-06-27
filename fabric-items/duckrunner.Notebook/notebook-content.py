9da40ac12fb14ac8acd079c1b75c4bdbe64ceafe
jose-marquez89 <58154690+jose-marquez89@users.noreply.github.com>
2026-06-27 16:56:18 -0500

    adds duckrun dbt runner code

--- fabric-items/duckrunner.Notebook/notebook-content.py
+++ fabric-items/duckrunner.Notebook/notebook-content.py
@@ -37,14 +37,13 @@ notebookutils.session.restartPython()
 
 import os
 
-slv_workspace = ''
-slv_lakehouse_art_id = ''
-gld_workspace = ''
-gld_lakehouse_art_id = ''
+workspace = '765a5a2b-3596-4e83-a39d-f2b211a8547c'
+slv_lakehouse_art_id = '7be5a932-1a70-4b53-8189-10051ebd3d56'
+gld_lakehouse_art_id = 'fa202274-d393-4677-b106-6db4304599b6'
 
-os.environ["BRZ_LH_PATH"] = '/lakehouse/default'
-os.environ["SLV_LH_PATH"] = f'abfss://{slv_workspace}@onelake.dfs.fabric.microsoft.com/{slv_lakehouse_art_id}'
-os.environ["GLD_LH_PATH"] = f'abfss://{gld_workspace}@onelake.dfs.fabric.microsoft.com/{gld_lakehouse_art_id}'
+os.environ["BRZ_LH_PATH"] = '/lakehouse/default/'
+os.environ["SLV_LH_PATH"] = f'abfss://{workspace}@onelake.dfs.fabric.microsoft.com/{slv_lakehouse_art_id}'
+os.environ["GLD_LH_PATH"] = f'abfss://{workspace}@onelake.dfs.fabric.microsoft.com/{gld_lakehouse_art_id}'
 
 # METADATA ********************
 
