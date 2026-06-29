15200c7cefb462195f5cdd4796a4f280e1318f37
Jose Marquez <jlmarquez89@gmail.com>
2026-06-29 18:31:45 -0500

    moves latitude and longitude to fact table

--- dbt/models/marts/fact_collision.sql
+++ dbt/models/marts/fact_collision.sql
@@ -1,8 +1,8 @@
-{{ config(merge_schema='true') }}
+{{ config(schema_mode='overwrite') }}
 
 SELECT
-    collision_id AS crash_id,
-    crash_timestamp,
+    collision_id,
+    crash_timestamp AS collision_timestamp,
     number_of_persons_injured AS persons_injured,
     number_of_persons_killed AS persons_killed,
     number_of_pedestrians_injured AS pedestrians_injured,
@@ -11,9 +11,9 @@ SELECT
     number_of_cyclist_killed AS cyclists_killed,
     number_of_motorist_injured AS motorists_injured,
     number_of_motorist_killed AS motorists_killed,
+    latitude,
+    longitude,
     {{ create_int_sk([
-        'latitude',
-        'longitude',
         'location',
         'borough',
         'on_street_name',
@@ -22,7 +22,7 @@ SELECT
     }} AS dim_location_sk,
     EXTRACT(YEAR FROM crash_date) * 10000 +
     EXTRACT(MONTH FROM crash_date) * 100 +
-    EXTRACT(DAY FROM crash_date) AS dim_date_sk,
+    EXTRACT(DAY FROM crash_date) AS collision_date_sk,
     CURRENT_TIMESTAMP AS load_date
 FROM {{ ref('collisions') }}
 ORDER BY crash_timestamp;
